6abda4cb3cdf842fb430688881c7ff0c04bdb04e
John Garcia <john.garcia144@gmail.com>
2020-07-12 15:50:57 -0600

    more string additions

--- Develop/script.js
+++ Develop/script.js
@@ -4,7 +4,7 @@ document.querySelector("#generate").addEventListener("click", writePassword);
 const upperCaseCharCode = arrayFromLowToHigh(65, 90)
 const lowerCaseCharCode = arrayFromLowToHigh(97, 122)
 const numbersCharCode = arrayFromLowToHigh(48, 57)
-const symbolCaseCharCode = arrayFromLowToHigh(33, 47).concat(
+const symbolsCharCode = arrayFromLowToHigh(33, 47).concat(
   arrayFromLowToHigh(58, 64)
 ).concat(
   arrayFromLowToHigh(91,96)
@@ -20,16 +20,24 @@ function arrayFromLowToHigh(low, high) {
   return array
 }
 
-
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
+function writePassword(characterAmount, includeUpperCase, includeNumbers, includeSymbols) {
+  let charCodes = lowerCaseCharCode
+  if (includeUpperCase) charCodes = charCodes.concat(upperCaseCharCode)
+  if (includeNumbers) charCodes = charCodes.concat(numbersCharCode)
+  if (includeSymbols) charCodes = charCodes.concat(symbolsCharCode)
+  
+  const passwordCharacters = []
+  for (let i = 0; i < characterAmount, i++) {
+    const characterCode = charCodes[Math.floor(Math.random() * charCodes.length)]
+    passwordCharacters.push(String.fromCharCode(characterCode))
+  }
+  return passwordCharacters.join("")
+}
+var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
-}
-
 // Add event listener to generate button
 generateB.addEventListener("click", writePassword);
 
