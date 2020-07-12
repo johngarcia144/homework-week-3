f79203ad4e04e8c27528259794aa52740cf9984e
John Garcia <john.garcia144@gmail.com>
2020-07-12 16:37:53 -0600

    new array with char

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,15 @@
 // Assignment Code
 document.querySelector("#generate").addEventListener("click", writePassword);
 
+var symbolsChar = ["!", "&", "#", ",", "*", "+", "$", ".", "/", "<", ">", "?","~", "%"];
+var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+const upperCaseCharCode = arrayFromLowToHigh(65, 90)
+const lowerCaseCharCode = arrayFromLowToHigh(97, 122)
+const numbersCharCode = arrayFromLowToHigh(48, 57)
+const symbolsCharCode = arrayFromLowToHigh(33, 47).concat(
 var upperCaseCharCode = arrayFromLowToHigh(65, 90);
 var lowerCaseCharCode = arrayFromLowToHigh(97, 122);
 var numbersCharCode = arrayFromLowToHigh(48, 57);
@@ -10,6 +19,7 @@ var symbolsCharCode = arrayFromLowToHigh(33, 47).concat(
   arrayFromLowToHigh(91,96)
 ).concat(
   arrayFromLowToHigh(123, 126)
+)
 );
 
 var confirmLength = "";
@@ -26,6 +36,7 @@ function arrayFromLowToHigh(low, high) {
   return array
 }
 
+// Write password to the #password input
 function writePassword(characterAmount, includeUpperCase, includeNumbers, includeSymbols) {
   let charCodes = lowerCaseCharCode
   if (includeUpperCase) charCodes = charCodes.concat(upperCaseCharCode)
@@ -46,5 +57,4 @@ var password = generatePassword();
   passwordText.value = password;
 // Add event listener to generate button
 generateB.addEventListener("click", writePassword);
-
 alert()
