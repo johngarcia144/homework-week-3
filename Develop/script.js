39b5b90c1d46d4856669747095f4fae64480b9ad
John Garcia <john.garcia144@gmail.com>
2020-07-12 15:20:35 -0600

    added char code array

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,26 @@
 // Assignment Code
 document.querySelector("#generate").addEventListener("click", writePassword);
 
+const upperCaseCharCode = arrayFromLowToHigh(65, 90)
+const lowerCaseCharCode = arrayFromLowToHigh(97, 122)
+const numbersCharCode = arrayFromLowToHigh(48, 57)
+const symbolCaseCharCode = arrayFromLowToHigh(33, 47).concat(
+  arrayFromLowToHigh(58, 64)
+).concat(
+  arrayFromLowToHigh(91,96)
+).concat(
+  arrayFromLowToHigh(123, 126)
+)
+
+function arrayFromLowToHigh(low, high) {
+  const array = []
+  for ( let i = low; i <= high; i++) {
+    array.push(i)
+  }
+  return array
+}
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
